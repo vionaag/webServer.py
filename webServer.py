0c7b919f02f04094cf0f3f0bd001cf014a825851
vionaag <va2213@nyu.edu>
2025-09-27 18:20:04 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -23,41 +23,32 @@ def webServer(port=13331):
       #opens the client requested file. 
       f = open(filename[1:], "rb")  # fill in start: open file in binary mode # fill in end
       
-      #This variable can store the headers you want to send for any valid or invalid request. 
-      #What header should be sent for a response that is ok?    
-      #Fill in start 
+      # 200 OK headers
       header = b"HTTP/1.1 200 OK\r\n"
-      #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
-      #Complete header must end with \r\n\r\n
-      header += outputdata + b"\r\n"
-      #Fill in end
+      header += b"Server: SimpleWebServer\r\n"
+      header += b"Connection: close\r\n"
+      header += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
                
       body = b""
       for i in f: #for line in file
         body += i  # Fill in start - append file contents # Fill in end 
         
-      #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command
-      # Fill in start
       connectionSocket.send(header + body)
-      # Fill in end
         
       connectionSocket.close() #closing the connection socket
       
     except Exception as e:
-      # Send response message for invalid request due to the file not being found (404)
-      # Remember the format you used in the try: block!
-      #Fill in start
-      errorHeader = b"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+      # 404 Not Found headers
+      errorHeader = b"HTTP/1.1 404 Not Found\r\n"
+      errorHeader += b"Server: SimpleWebServer\r\n"
+      errorHeader += b"Connection: close\r\n"
+      errorHeader += b"Content-Type: text/html\r\n\r\n"
       errorBody = b"<html><head></head><body><h1>404 Not Found</h1></body></html>"
       connectionSocket.send(errorHeader + errorBody)
-      #Fill in end
 
       #Close client socket
-      #Fill in start
       connectionSocket.close()
-      #Fill in end
 
   # DO NOT UNCOMMENT THESE WHEN SUBMITTING
   #serverSocket.close()
